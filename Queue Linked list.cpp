d61c05894483a9fce5f3ac1f4108f70e83890e12
Jaswanth83 <jaswanthaatukuri139@gmail.com>
2023-08-25 10:15:32 +0530

    Create Queue Linked list.cpp

---
+++ Queue Linked list.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 100
+int n;
+int queue[max];
+struct node{
+	int data;
+	struct node*next;
+};
+struct node*front=NULL;
+struct node*rear=NULL;
+void enqueue()
+{
+	struct node*ptr,*next;
+	
+}
